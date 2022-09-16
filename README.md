9ca6c3dcab53fc53e6f8b5b0d011f0c80a87aebd
iCE <pt2ukk@gmail.com>
2022-09-16 04:53:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# ![logo](https://raw.githubusercontent.com/azerothcore/azerothcore.github.io/master/images/logo-github.png) AzerothCore
+
+## Mod-MultiVendor
+
+## Description
+
+- Multi-Vendor NPCS iLevel = 284  > 277 > 284 > 264 > 245
+- Weapons, Armors & Accessories
+- Easy to edit shop list via database
+
+
+
+## Requirements
+
+- Latest AzerothCore Wotlk commit [50ae8e9](https://github.com/azerothcore/acore-cms/commit/50ae8e95e7e2b68ccf1e255ee368a590cf884596) or newer
+
+## Installation
+
+1. Simply place the module under the `modules` directory of your AzerothCore source. 
+2. Re-run CMake and rebuild the AzerothCore source.
+3. Run Sqls on to the world db.
+## Edit module configuration (optional)
+
+
+### Credits ###
+------------------------------------------------------------------------------------------------------------------
+- [HolyDEEW (Holy)](https://github.com/WoWHellgarve-HolyDeeW)
+
---
+++ src/MultiVendor.cpp
@@ -0,0 +1,293 @@
+/*
+ *MIT License
+ *
+ *Copyright (c) 2022 Azerothcore & WoWHellgarve
+ *
+ *Permission is hereby granted, free of charge, to any person obtaining a copy
+ *of this software and associated documentation files (the "Software"), to deal
+ *in the Software without restriction, including without limitation the rights
+ *to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *copies of the Software, and to permit persons to whom the Software is
+ *furnished to do so, subject to the following conditions:
+ *
+ *The above copyright notice and this permission notice shall be included in all
+ *copies or substantial portions of the Software.
+ *
+ *THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *SOFTWARE.
+ *
+ * Multi-Vendor Module
+ * Original Module From WoWHellgarve 
+ * Written for Azerothcore & WoWHellgarve 335 By Developer HolyDEEW
+ * Modified for Azerothcore
+ */
+
+#include "Player.h"
+#include "ScriptMgr.h"
+#include "ScriptedGossip.h"
+
+
+/*-- DATA NPC_TEMPLATE
+SET
+@NpcEntry 		:= 666666
+@NpcName		:= "Multi-Vendor"
+@NpcSubname		:= "WoWHellgarve Vendor"
+@NpcDisplayID   := 1298
+*/
+
+#define GOSSIP_HELLO    "Greetings, it is time to gear up SUCKERS! ."
+//277_284-------------------------------------
+#define GOSSIP_TEXT_BROWSE_WEAPONS_277_284      "|TInterface/ICONS/Inv_mace_116:22:22:-18|t[Let me browse your Weapons]"
+#define GOSSIP_TEXT_BROWSE_ARMOR_277_284        "|TInterface/ICONS/inv_chest_cloth_04:24:24:-18|t[Show me T10,5 Armor]"
+#define GOSSIP_TEXT_BROWSE_ACCESSOIRES_277_284  "|TInterface/ICONS/inv_chest_cloth_04:24:24:-18|t[Show me off set pieces]"
+//264-------------------------------------
+#define GOSSIP_TEXT_BROWSE_WEAPONS_264          "|TInterface/ICONS/Inv_mace_116:22:22:-18|t[Let me browse your Weapons]"
+#define GOSSIP_TEXT_BROWSE_ARMOR_264            "|TInterface/ICONS/inv_chest_cloth_04:24:24:-18|t[Show me T10 Armor]"
+#define GOSSIP_TEXT_BROWSE_ACCESSOIRES_264      "|TInterface/ICONS/inv_chest_cloth_04:24:24:-18|t[Show me off set pieces]"
+//245 -------------------------------------
+#define GOSSIP_TEXT_BROWSE_ARMOR_245            "|TInterface/ICONS/inv_chest_cloth_04:24:24:-18|t[T9 Armor]"
+#define GOSSIP_TEXT_BROWSE_WEAPONS_245          "|TInterface/ICONS/Inv_mace_116:22:22:-18|t[Let me browse your Weapons]"
+#define GOSSIP_TEXT_BROWSE_ACCESSOIRES_245      "|TInterface/ICONS/inv_chest_cloth_04:24:24:-18|t[Show me off set pieces]"
+
+//npc_vendor database
+enum shop_list_id
+{
+    // T10,5 277_284
+    VENDOR_WEAPON_LIST_277_284 =  6666666, 
+    VENDOR_ARMOR_LIST_277_284  =  6666667, 
+    VENDOR_ACCESSOIRES_LIST_277_284 = 6666668,
+  
+    // 264
+    VENDOR_WEAPON_LIST_264 =  6666669, 
+    VENDOR_ARMOR_LIST_264 =  6666670, 
+    VENDOR_ACCESSOIRES_LIST_264 = 6666671,
+
+    // 245
+    VENDOR_WEAPON_LIST_245 =  6666672, 
+    VENDOR_ARMOR_LIST_245 =  6666673, 
+    VENDOR_ACCESSOIRES_LIST_245 = 6666674
+
+};
+
+
+/***********************/
+/*NPC_SCRIPTS *********/
+/**********************/
+
+class npc_multi_vendor_277_284 : public CreatureScript
+{
+public:
+    npc_multi_vendor_277_284() : CreatureScript("npc_multi_vendor_277_284") { }
+
+    bool OnGossipHello(Player* player, Creature* creature) override
+    { 
+        if (creature->IsVendor())
+            AddGossipItemFor(player, GOSSIP_ICON_CHAT, GOSSIP_HELLO, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF);
+
+            AddGossipItemFor(player, GOSSIP_ICON_VENDOR, GOSSIP_TEXT_BROWSE_WEAPONS_277_284, GOSSIP_ACTION_TRADE, 1);
+            AddGossipItemFor(player, GOSSIP_ICON_VENDOR, GOSSIP_TEXT_BROWSE_ARMOR_277_284, GOSSIP_ACTION_TRADE, 2);
+            AddGossipItemFor(player, GOSSIP_ICON_VENDOR, GOSSIP_TEXT_BROWSE_ACCESSOIRES_277_284, GOSSIP_ACTION_TRADE, 3);
+
+        SendGossipMenuFor(player, player->GetGossipTextId(creature), creature->GetGUID());
+        return true;
+    }
+
+    bool OnGossipSelect(Player* player, Creature* creature, uint32 /*sender*/, uint32 action) override
+    {
+        player->PlayerTalkClass->ClearMenus();
+
+        switch (action)
+        {
+         case 1: // WEAPONS
+            CloseGossipMenuFor(player);
+            if (player->IsInCombat())
+            {
+                CloseGossipMenuFor(player);
+                player->GetSession()->SendNotification("You are in combat!");
+                return false;
+            }
+            else if (player->getClassMask())
+            {
+                player->GetSession()->SendListInventory(creature->GetGUID(), VENDOR_WEAPON_LIST_277_284);
+            }            
+             break;
+         case 2: // ARMOR
+             CloseGossipMenuFor(player);
+             if (player->IsInCombat())
+             {
+                 CloseGossipMenuFor(player);
+                 player->GetSession()->SendNotification("You are in combat!");
+                 return false;
+             }
+             else if (player->getClassMask())
+             {
+               player->GetSession()->SendListInventory(creature->GetGUID(), VENDOR_ARMOR_LIST_277_284);
+             }
+             break;
+         case 3: // ACCESSOIRES
+             CloseGossipMenuFor(player);
+             if (player->IsInCombat())
+             {
+                 CloseGossipMenuFor(player);
+                 player->GetSession()->SendNotification("You are in combat!");
+                 return false;
+             }
+             else if (player->getClassMask())
+             {
+                player->GetSession()->SendListInventory(creature->GetGUID(), VENDOR_ACCESSOIRES_LIST_277_284);
+             }
+             break;
+        }
+        return true;
+    }
+};
+
+class npc_multi_vendor_264 : public CreatureScript
+{
+public:
+    npc_multi_vendor_264() : CreatureScript("npc_multi_vendor_264") { }
+
+    bool OnGossipHello(Player* player, Creature* creature) override
+    { 
+        if (creature->IsVendor())
+            AddGossipItemFor(player, GOSSIP_ICON_CHAT, GOSSIP_HELLO, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF);
+
+            AddGossipItemFor(player, GOSSIP_ICON_VENDOR, GOSSIP_TEXT_BROWSE_WEAPONS_264, GOSSIP_ACTION_TRADE, 1);
+            AddGossipItemFor(player, GOSSIP_ICON_VENDOR, GOSSIP_TEXT_BROWSE_ARMOR_264, GOSSIP_ACTION_TRADE, 2);
+            AddGossipItemFor(player, GOSSIP_ICON_VENDOR, GOSSIP_TEXT_BROWSE_ACCESSOIRES_264, GOSSIP_ACTION_TRADE, 3);
+
+        SendGossipMenuFor(player, player->GetGossipTextId(creature), creature->GetGUID());
+        return true;
+    }
+
+    bool OnGossipSelect(Player* player, Creature* creature, uint32 /*sender*/, uint32 action) override
+    {
+        player->PlayerTalkClass->ClearMenus();
+
+        switch (action)
+        {
+         case 1: // WEAPONS
+            CloseGossipMenuFor(player);
+            if (player->IsInCombat())
+            {
+                CloseGossipMenuFor(player);
+                player->GetSession()->SendNotification("You are in combat!");
+                return false;
+            }
+            else if (player->getClassMask())
+            {
+                player->GetSession()->SendListInventory(creature->GetGUID(), VENDOR_WEAPON_LIST_264);
+            }            
+             break;
+         case 2: // ARMOR
+             CloseGossipMenuFor(player);
+             if (player->IsInCombat())
+             {
+                 CloseGossipMenuFor(player);
+                 player->GetSession()->SendNotification("You are in combat!");
+                 return false;
+             }
+             else if (player->getClassMask())
+             {
+               player->GetSession()->SendListInventory(creature->GetGUID(), VENDOR_ARMOR_LIST_264);
+             }
+             break;
+         case 3: // ACCESSOIRES
+             CloseGossipMenuFor(player);
+             if (player->IsInCombat())
+             {
+                 CloseGossipMenuFor(player);
+                 player->GetSession()->SendNotification("You are in combat!");
+                 return false;
+             }
+             else if (player->getClassMask())
+             {
+                player->GetSession()->SendListInventory(creature->GetGUID(), VENDOR_ACCESSOIRES_LIST_264);
+             }
+             break;
+        }
+        return true;
+    }
+};
+
+
+class npc_multi_vendor_245 : public CreatureScript
+{
+public:
+    npc_multi_vendor_245() : CreatureScript("npc_multi_vendor_245") { }
+
+    bool OnGossipHello(Player* player, Creature* creature) override
+    { 
+        if (creature->IsVendor())
+            AddGossipItemFor(player, GOSSIP_ICON_CHAT, GOSSIP_HELLO, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF);
+
+            AddGossipItemFor(player, GOSSIP_ICON_VENDOR, GOSSIP_TEXT_BROWSE_WEAPONS_245, GOSSIP_ACTION_TRADE, 1);
+            AddGossipItemFor(player, GOSSIP_ICON_VENDOR, GOSSIP_TEXT_BROWSE_ARMOR_245, GOSSIP_ACTION_TRADE, 2);
+            AddGossipItemFor(player, GOSSIP_ICON_VENDOR, GOSSIP_TEXT_BROWSE_ACCESSOIRES_245, GOSSIP_ACTION_TRADE, 3);
+
+        SendGossipMenuFor(player, player->GetGossipTextId(creature), creature->GetGUID());
+        return true;
+    }
+
+    bool OnGossipSelect(Player* player, Creature* creature, uint32 /*sender*/, uint32 action) override
+    {
+        player->PlayerTalkClass->ClearMenus();
+
+        switch (action)
+        {
+         case 1: // Weapons
+            CloseGossipMenuFor(player);
+            if (player->IsInCombat())
+            {
+                CloseGossipMenuFor(player);
+                player->GetSession()->SendNotification("You are in combat!");
+                return false;
+            }
+            else if (player->getClassMask())
+            {
+                player->GetSession()->SendListInventory(creature->GetGUID(), VENDOR_WEAPON_LIST_245);
+            }            
+             break;
+         case 2: // Armors
+             CloseGossipMenuFor(player);
+             if (player->IsInCombat())
+             {
+                 CloseGossipMenuFor(player);
+                 player->GetSession()->SendNotification("You are in combat!");
+                 return false;
+             }
+             else if (player->getClassMask())
+             {
+               player->GetSession()->SendListInventory(creature->GetGUID(), VENDOR_ARMOR_LIST_245);
+             }
+             break;
+         case 3: // Specials
+             CloseGossipMenuFor(player);
+             if (player->IsInCombat())
+             {
+                 CloseGossipMenuFor(player);
+                 player->GetSession()->SendNotification("You are in combat!");
+                 return false;
+             }
+             else if (player->getClassMask())
+             {
+                player->GetSession()->SendListInventory(creature->GetGUID(), VENDOR_ACCESSOIRES_LIST_245);
+             }
+             break;
+        }
+        return true;
+    }
+};
+
+
+void AddMultiVendor_scripts()
+{
+   new npc_multi_vendor_277_284();
+   new npc_multi_vendor_264();
+   new npc_multi_vendor_245();
+}
